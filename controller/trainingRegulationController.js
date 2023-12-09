277604d31e49e741c7d72d7dabe544c5607a9aa7
VKH2801 <20521352@gm.uit.edu.vn>
2023-12-09 22:02:52 +0700

    Refactor - Training Reg API

--- controller/trainingRegulationController.js
+++ controller/trainingRegulationController.js
@@ -19,13 +19,6 @@ const getAllTrainingReg = async (req, res) => {
 
 const getTrainingById = async (req, res) => {
   try {
-    if (!req.params.id) {
-      res.status(401).json({
-        code: 401,
-        message: "Missing Id in request parameter",
-      });
-      return;
-    }
     let data = await TrainingRegulation.findOne({ _id: req.params.id });
     res.status(200).json({
       code: 200,
@@ -43,15 +36,14 @@ const getTrainingById = async (req, res) => {
 
 const createNewTrainingReg = async (req, res) => {
   try {
-    let { id, title, createBy } = req.body;
-    if (!title || !id || !createBy) {
+    let { title, content, createBy } = req.body;
+    if (!title || !content || !createBy) {
       res.status(401).json({
         code: 401,
         message: "Missing data",
       });
       return;
     }
-    const findTraining = await TrainingRegulation.findOne({ id: id });
     const findUserIdCreate = await UserSchema.findOne({ _id: createBy });
     if (!findUserIdCreate) {
       return res.status(405).json({
@@ -59,28 +51,20 @@ const createNewTrainingReg = async (req, res) => {
         message: "Invalid User",
       });
     }
-    if (!findTraining) {
-      console.log(id, title);
-      let trainingReg = new TrainingRegulation({
-        id: id,
-        title: title,
-        idUserLatestEdit: findUserIdCreate,
-        listIdUserEdited: [createBy],
-        createdBy: findUserIdCreate,
-      });
-      let result = await trainingReg.save();
-      res.status(200).json({
-        code: 200,
-        data: lodash.omit(result.toObject()),
-        message: "OK",
-      });
-      return;
-    } else {
-      res.status(400).json({
-        code: 400,
-        message: "Existing training regulation id",
-      });
-    }
+    let trainingReg = new TrainingRegulation({
+      title: title,
+      content: content,
+      idUserLatestEdit: findUserIdCreate,
+      listIdUserEdited: [findUserIdCreate],
+      createdBy: findUserIdCreate,
+    });
+    let result = await trainingReg.save();
+    res.status(200).json({
+      code: 200,
+      data: result,
+      message: "OK",
+    });
+    return;
   } catch (err) {
     res.status(500).json({
       code: 500,
@@ -92,7 +76,6 @@ const createNewTrainingReg = async (req, res) => {
 const updateTrainingReg = async (req, res) => {
   try {
     let trainingReg = await TrainingRegulation.findOne({ _id: req.params.id });
-    //console.log(trainingReg);
     let data = req.body;
 
     if (!trainingReg) {
@@ -110,33 +93,14 @@ const updateTrainingReg = async (req, res) => {
       return;
     }
 
-    if (data.listIdUserEdited) {
-      return res.status(402).json({
-        code: 402,
-        message: "Unable to update list user edited",
-      });
-    }
-
-    if (data.createBy) {
-      return res.status(402).json({
-        code: 402,
-        message: "Unable to update id user created",
-      });
-    }
+    data.listIdUserEdited = trainingReg.listIdUserEdited;
+    data.createBy = trainingReg.createBy;
 
     if (!data.idUserLatestEdit) {
       return res.status(401).json({
         code: 401,
-        message: "Missing data user latest update",
+        message: "Missing data user for update",
       });
-    }
-
-    if (data.id !== trainingReg.id) {
-      res.status(402).json({
-        code: 402,
-        message: "Unable update id - It a unique key",
-      });
-      return;
     }
 
     const findUserIdEdit = await UserSchema.findOne({
@@ -149,40 +113,19 @@ const updateTrainingReg = async (req, res) => {
       });
     }
 
-    req.body.listIdUserEdited = trainingReg.listIdUserEdited;
-    let _listUserEdited = trainingReg.listIdUserEdited;
-
-    if (!_listUserEdited.includes(data.idUserLatestEdit)) {
-      _listUserEdited.push(findUserIdEdit._id);
+    if (!data.listIdUserEdited.includes(data.idUserLatestEdit)) {
+      data.listIdUserEdited.push(data.idUserLatestEdit);
     }
 
-    req.body.idUserLatestEdit = trainingReg.idUserLatestEdit;
-    req.body.createBy = trainingReg.createdBy;
-
-    const updatedTrainingReg = await TrainingRegulation.findByIdAndUpdate(
+    const result = await TrainingRegulation.findByIdAndUpdate(
       req.params.id,
-      { $set: lodash.omit(req.body, "id") },
+      { $set: lodash.omit(data) },
       { new: true }
     );
 
-    const findUserCreated = await UserSchema.findOne({
-      _id: updatedTrainingReg.createdBy,
-    });
-
-    const resData = {
-      _id: updatedTrainingReg._id,
-      __v: updatedTrainingReg.__v,
-      id: updatedTrainingReg.id,
-      title: updatedTrainingReg.title,
-      idUserLatestEdit: findUserIdEdit,
-      listIdUserEdited: updatedTrainingReg.listIdUserEdited,
-      createdBy: findUserCreated
-        ? findUserCreated
-        : updatedTrainingReg.createdBy,
-    };
     res.status(200).json({
       code: 200,
-      data: resData,
+      data: result,
       message: "OK",
     });
   } catch (err) {
--- models/trainingRegulationModel.js
+++ models/trainingRegulationModel.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const UserSchema = require('./userModel');
 
 const trainingRegulation = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-    //default: 1,
-  },
   title: {
+    type: String,
+    required: true,
+    //default: 'Training registration',
+  },
+  content: {
     type: String,
     required: true,
     //default: 'Chương trình đào tạo cử nhân chính quy ngành Kỹ thuật Phần mềm được thực hiện theo quy chế, quy định đào tạo hiện hành của Trường Đại học Công nghệ Thông tin.',
