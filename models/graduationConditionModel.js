c280d446f8a7ab85a799ed5c56ce4137b213463b
VKH2801 <20521352@gm.uit.edu.vn>
2023-12-11 10:59:54 +0700

    Refactor - Graduation Conditions API

--- models/graduationConditionModel.js
+++ models/graduationConditionModel.js
@@ -1,23 +1,35 @@
 const mongoose = require("mongoose");
 
-const graduationConditions = mongoose.Schema({
-  id: {
-    type: Number,
-    require: true,
-    //default: 1
+const graduationConditions = mongoose.Schema(
+  {
+    content: {
+      type: String,
+      require: true,
+      //default: 'Sinh viên đã tích lũy tối thiểu 130 tín chỉ, đã hoàn thành các môn học bắt buộc của chương trình đào tạo tương ứng với chuyên ngành.'
+    },
+    idOverView: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Overview",
+      require: true,
+      //default: '7480103',
+      // This field is the id of overview model to reference
+    },
+    idUserLatestEdit: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    listIdUserEdited: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  name: {
-    type: String,
-    require: true,
-    //default: 'Sinh viên đã tích lũy tối thiểu 130 tín chỉ, đã hoàn thành các môn học bắt buộc của chương trình đào tạo tương ứng với chuyên ngành.'
-  },
-  idOverView: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Overview',
-    require: true,
-    //default: '7480103',
-    // This field is the id of overview model to reference
-  }
-}, { timestamps: true })
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('GraduationCondition', graduationConditions);
+module.exports = mongoose.model("GraduationCondition", graduationConditions);
--- routes/graduationConditionRoute.js
+++ routes/graduationConditionRoute.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const graduationCondition = require("../controller/graduationConditionController");
 
-router.get('/', graduationCondition.getAllGraduationConditions);
-router.get('/get/:id', graduationCondition.getGraduationConditionsById);
-router.post('/new', graduationCondition.createGraduationCondition);
-router.put('/:id', graduationCondition.updateGraduationCondition);
-router.delete('/deleteAll', graduationCondition.deleteAllGraduationCondition);
-router.delete('/delete', graduationCondition.deleteGraduationById);
+router.get("/", graduationCondition.getAllGraduationConditions);
+router.get("/get/:id", graduationCondition.getGraduationConditionsById);
+router.post("/new", graduationCondition.createGraduationCondition);
+router.put("/:id", graduationCondition.updateGraduationCondition);
+router.delete("/deleteAll", graduationCondition.deleteAllGraduationCondition);
+router.delete("/delete/:id", graduationCondition.deleteGraduationById);
 
 module.exports = router;
