6a07b2783a0a715b052014a5dfa40b186b67b482
VKH2801 <20521352@gm.uit.edu.vn>
2024-01-14 23:19:56 +0700

    Refactor

--- models/classificationScaleModel.js
+++ models/classificationScaleModel.js
@@ -1,48 +1,54 @@
 const mongoose = require("mongoose");
 
-const classificationScale = mongoose.Schema({
-  code: {
-    type: String,
-    require: true,
+const classificationScale = mongoose.Schema(
+  {
+    code: {
+      type: String,
+      require: true,
+    },
+    level: {
+      type: Number,
+      require: true,
+      //default: 1,2,...
+    },
+    title: {
+      type: String,
+      require: true,
+      //default: Nhớ, Hiểu, ...
+      default: "",
+    },
+    discription: {
+      type: String,
+      require: true,
+      //default: 'Là khả năng ghi nhận và truy xuất lại các kiến thức, thông tin đã tiếp nhận; thể hiện qua việc có thể nhắc lại các kiến thức, thông tin đó.'
+    },
+    type: {
+      type: String,
+      enum: ["awareness", "skill", "attitude"],
+      default: "awareness",
+    },
+    // idOutputType: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'OutputType',
+    //   required: true,
+    //   //default: '654b72698ba535dccbb0f7be'
+    // },
+    idUserLatestEdit: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    listIdUserEdited: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  level: {
-    type: Number,
-    require: true,
-    //default: 1,2,...
-  },
-  title: {
-    type: String,
-    require: true,
-    //default: Nhớ, Hiểu, ...
-  },
-  discription: {
-    type: String,
-    require: true,
-    //default: 'Là khả năng ghi nhận và truy xuất lại các kiến thức, thông tin đã tiếp nhận; thể hiện qua việc có thể nhắc lại các kiến thức, thông tin đó.'
-  },
-  type: {
-    type: String,
-    enum: ['awareness', 'skill', 'attitude'],
-    default: 'awareness'
-  },
-  // idOutputType: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'OutputType',
-  //   required: true,
-  //   //default: '654b72698ba535dccbb0f7be'
-  // },
-  idUserLatestEdit: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  listIdUserEdited: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-}, { timestamps: true })
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('ClassificationScale', classificationScale);
+module.exports = mongoose.model("ClassificationScale", classificationScale);
--- models/userModel.js
+++ models/userModel.js
@@ -1,39 +1,61 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            required: true,
-            default: 'user',
-        },
-        gender: {
-            type: String,
-            //default: 'male' | 'female',
-        },
-        lastName: {
-            type: String,
-        },
-        firstName: {
-            type: String,
-        },
-        phoneNumber: {
-            type: String,
-        },
-        avatar: {
-            type: String,
-        }
+  {
+    email: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      required: true,
+      default: "user",
+    },
+    gender: {
+      type: String,
+      //default: 'male' | 'female',
+    },
+    lastName: {
+      type: String,
+    },
+    firstName: {
+      type: String,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    pdfData: {
+      overview: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Overview",
+      },
+      enroll: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Enroll",
+      },
+      outputType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "OutputType",
+      },
+      classifyScale: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ClassificationScale",
+      },
+      subjectCombination: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubjectCombination",
+      },
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
